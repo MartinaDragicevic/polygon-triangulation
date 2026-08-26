76922035770292e8b1eedb5074a39332a9488a03
Petar Milanovic <petarrmilanovic@gmail.com>
2026-08-26 12:48:28 +0200

    Use Point type in triangulation

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,14 @@
 module Main (main) where
 
-import PolygonGenerator
-import System.Random (mkStdGen)
 import Triangulation
+import Types
 
 main :: IO ()
-main = do
-    let generator = mkStdGen 42
-    let (points, _) = generateUniquePoints 10 (0, 100) generator
-    print points
-    print (triangulateConvex ([1, 2, 3, 4, 5] :: [Int]))
+main =
+    print
+        (triangulateConvex
+            [ Point 0 0
+            , Point 4 0
+            , Point 4 4
+            , Point 0 4
+            ])
--- app/Triangulation.hs
+++ app/Triangulation.hs
@@ -1,14 +1,15 @@
 module Triangulation where
 
-type Triangle a = (a, a, a)
+import Types (Point)
 
-triangulateConvex :: [a] -> [Triangle a]
+type Triangle = (Point, Point, Point)
+
+triangulateConvex :: [Point] -> [Triangle]
 triangulateConvex (first : second : third : rest) =
     (first, second, third) : buildTriangles first third rest
   where
     buildTriangles _ _ [] = []
     buildTriangles anchor previous (next : remaining) =
-        (anchor, previous, next)
-            : buildTriangles anchor next remaining
+        (anchor, previous, next) : buildTriangles anchor next remaining
 
 triangulateConvex _ = []
