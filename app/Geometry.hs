cef8db4c3c61c6ca019060f07636aee9f261fa3e
Martina Dragicevic <m.dragicevicc44@gmail.com>
2026-09-04 16:20:21 +0200

    Add simple polygon validation

--- app/Geometry.hs
+++ app/Geometry.hs
@@ -25,3 +25,28 @@ segmentsIntersect p1 p2 p3 p4 =
     in oppositeSigns o1 o2 && oppositeSigns o3 o4
   where
     oppositeSigns a b = (a > 0 && b < 0) || (a < 0 && b > 0)
+
+polygonEdges :: Polygon -> [(Point, Point)]
+polygonEdges [] = []
+polygonEdges points = zip points (tail points ++ [head points])
+
+isSimplePolygon :: Polygon -> Bool
+isSimplePolygon polygon
+    | length polygon < 3 = False
+    | otherwise = not (any edgesIntersect edgePairs)
+  where
+    edges = polygonEdges polygon
+    indexedEdges = zip [0 ..] edges
+    edgeCount = length edges
+
+    edgePairs =
+        [ (edge1, edge2)
+        | (i, edge1) <- indexedEdges
+        , (j, edge2) <- indexedEdges
+        , i < j
+        , not (areAdjacent i j edgeCount)
+        ]
+
+    areAdjacent i j n = j == i + 1 || (i == 0 && j == n - 1)
+
+    edgesIntersect ((a, b), (c, d)) = segmentsIntersect a b c d
--- app/Main.hs
+++ app/Main.hs
@@ -5,15 +5,8 @@ import Types
 
 main :: IO ()
 main = do
-    let a = Point 0 0
-        b = Point 4 4
-        c = Point 0 4
-        d = Point 4 0
+    let simplePolygon = [Point 0 0, Point 4 0, Point 4 4, Point 0 4]
+        selfIntersectingPolygon = [Point 0 0, Point 4 4, Point 0 4, Point 4 0]
 
-    let e = Point 0 0
-        f = Point 4 0
-        g = Point 0 2
-        h = Point 4 2
-
-    print (segmentsIntersect a b c d)
-    print (segmentsIntersect e f g h)
+    print (isSimplePolygon simplePolygon)
+    print (isSimplePolygon selfIntersectingPolygon)
